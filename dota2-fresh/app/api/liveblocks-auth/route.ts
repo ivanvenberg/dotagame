1f3f8dc2a234233c506597f1ac1775a8d96d8eba
ivanvenberg <68998261+ivanvenberg@users.noreply.github.com>
2026-04-18 17:41:18 +0200

    Update route.ts

--- dota2-fresh/app/api/liveblocks-auth/route.ts
+++ dota2-fresh/app/api/liveblocks-auth/route.ts
@@ -1,12 +1,19 @@
 import { Liveblocks } from "@liveblocks/node";
 import { NextRequest, NextResponse } from "next/server";
 
-const liveblocks = new Liveblocks({ secret: process.env.LIVEBLOCKS_SECRET_KEY! });
-
 export async function POST(request: NextRequest) {
+  const liveblocks = new Liveblocks({
+    secret: process.env.LIVEBLOCKS_SECRET_KEY!,
+  });
+
   const { playerId, playerName, roomId } = await request.json();
-  if (!playerId || !playerName) return NextResponse.json({ error: "Missing info" }, { status: 400 });
-  const session = liveblocks.prepareSession(playerId, { userInfo: { name: playerName } });
+  if (!playerId || !playerName) {
+    return NextResponse.json({ error: "Missing info" }, { status: 400 });
+  }
+
+  const session = liveblocks.prepareSession(playerId, {
+    userInfo: { name: playerName },
+  });
   session.allow(roomId, session.FULL_ACCESS);
   const { status, body } = await session.authorize();
   return new NextResponse(body, { status });
